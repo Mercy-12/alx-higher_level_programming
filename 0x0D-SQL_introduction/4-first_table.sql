fe606245d986f4284421f1fd28fd87cc7f1921bb
Mercy-12 <mehretmeridt@gmail.com>
2022-08-09 15:38:51 -0700

    Full creation

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
--- Creates first table
--- Create first_table with id, name
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT,
-    name VARCHAR(256)
-) ENGINE=INNODB;
+-- Creates a table called first_table with values id and name.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
